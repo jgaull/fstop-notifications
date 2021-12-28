b687b1381fb1650fa50c11f0bd700c8e7682dc96
Jonathan Gaull <jonbeesh@gmail.com>
2021-12-27 18:30:22 -0800

    Added a bunch of features for integrations

--- README.md
+++ README.md
@@ -8,4 +8,6 @@
 - `docker-compose up` to run the DB locally
 - Visit http://localhost:4000/
 index.js to test
+- `npm start` to start the core services
+- `cd twitch-chat && npm start` to start the Twitch Chat integration
  
--- index.js
+++ index.js
@@ -18,11 +18,22 @@ const notificationSchema = new mongoose.Schema({
 const Notification = mongoose.model('Notification', notificationSchema);
 
 const userSchema = new mongoose.Schema({
-    name: String
+    name: String,
+    email: String,
+    password: String
 })
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.model('User', userSchema)
 
+const integrationSchema = new mongoose.Schema({
+    type: String,
+    user: mongoose.ObjectId,
+    integrationUsername: String,
+    integrationAuthToken: String,
+    integrationSettings: String
+})
+
+const Integration = mongoose.model('Integration', integrationSchema)
 
 const { ApolloServer, gql } = require('apollo-server');
 
@@ -32,12 +43,31 @@ const { ApolloServer, gql } = require('apollo-server');
 const typeDefs = gql`
 
     type User {
-        name: String
+        name: String,
+        email: String,
+        password: String
         _id: ID
     }
 
     input UserInput {
         name: String
+    }
+
+    type Integration {
+        _id: ID
+        type: String
+        user: ID
+        integrationUsername: String
+        integrationAuthToken: String
+        integrationSettings: String
+    }
+
+    input IntegrationInput {
+        type: String
+        user: ID
+        integrationUsername: String
+        integrationAuthToken: String
+        integrationSettings: String
     }
 
     type BulkDeletionResult {
@@ -78,6 +108,9 @@ const typeDefs = gql`
         notificationsForUser(userId: ID): [Notification]
 
         users: [User]
+
+        integrations(query: IntegrationInput): [Integration]
+        integration(id: ID): Integration
     }
 
     type Mutation {
@@ -86,6 +119,11 @@ const typeDefs = gql`
         deleteNotifications: BulkDeletionResult
 
         createUser(user: UserInput): User
+
+        createIntegration(input: IntegrationInput): Integration
+        updateIntegration(id: ID, input: IntegrationInput): Integration
+        deleteIntegration(id: ID): Integration
+        deleteIntegrations: BulkDeletionResult
     }
 `
 
@@ -95,25 +133,35 @@ const resolvers = {
     Query: {
         notifications: async () => Notification.find({}),
         notificationsForUser: async (nothing, params) => await Notification.find({ user: params.userId }),
-        users: async () => User.find({})
+        users: async () => User.find({}),
+        integrations: async (nothing, params) => {
+            return Integration.find(params.query)
+        },
+        integration: async (nothing, params) => Integration.findById(params.id)
     },
     Mutation: {
         createNotification: async (nothing, params) => {
             const notification = new Notification(params.notification)
-            console.log(`creating a new notification: ${JSON.stringify(notification)}`)
             return notification.save()
         },
-        deleteNotification: async (nothing, params) => {
-            return Notification.findByIdAndDelete(params.id)
-        },
-        deleteNotifications: async (nothing, params) => {
-            return Notification.deleteMany({})
-        },
+        deleteNotification: async (nothing, params) => Notification.findByIdAndDelete(params.id),
+        deleteNotifications: async () => Notification.deleteMany({}),
+
         createUser: async (nothing, params) => {
             const user = new User(params.user)
-            console.log(`creating a new user: ${JSON.stringify(user)}`)
             return user.save()
-        }
+        },
+
+        createIntegration: async (nothing, params) => {
+            const integration = new Integration(params.input)
+            return integration.save()
+        },
+        updateIntegration: async (nothing, params) => {
+            const integration = new Integration(params.input)
+            return integration.updateOne({ _id: params.id }, params.input)
+        },
+        deleteIntegration: async (nothing, params) => Integration.findByIdAndDelete(params.id),
+        deleteIntegrations: async () => Integration.deleteMany({})
     }
 };
 
