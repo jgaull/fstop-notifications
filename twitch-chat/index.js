cf8b2ad2ef39e06562d3255c1c2ece01b86706a1
Jonathan Gaull <jonbeesh@gmail.com>
2021-12-27 16:29:45 -0800

    Twitch chat monitoring is hooked up

--- twitch-chat/index.js
+++ twitch-chat/index.js
@@ -4,10 +4,6 @@ const { request, gql } = require('graphql-request')
 
 // Define configuration options
 const opts = {
-  identity: {
-    username: process.env.BOT_USERNAME,
-    password: process.env.OAUTH_TOKEN
-  },
   channels: [
     process.env.CHANNEL_NAME
   ]
@@ -17,58 +13,60 @@ const opts = {
 const client = new tmi.client(opts)
 
 // Register our event handlers (defined below)
-client.on('message', (target, context, msg, self) => {
-  try {
-    onMessageHandler(target, console, msg, self)
-  }
-  catch (error) {
-    console.log(error.stack)
-  }
-})
-
+client.on('message', onMessageHandler)
 client.on('connected', onConnectedHandler)
 
 // Connect to Twitch:
 client.connect()
 
 // Called every time a message comes in
-async function onMessageHandler (target, context, msg, self) {
+async function onMessageHandler (target, context, message, self) {
+  /*
   console.log('new chat message:')
   console.log(`   target: ${JSON.stringify(target)}`)
   console.log(`   context: ${JSON.stringify(context)}`)
-  console.log(`   msg: ${JSON.stringify(msg)}`)
+  console.log(`   msg: ${JSON.stringify(message)}`)
   console.log(`   self: ${JSON.stringify(self)}`)
+  */
 
   const query = gql`
-  mutation createNotification {
-    createNotification(notification: {
-        title: "bikecurious"
-        message: "hello"
-        type: "Info"
-        data: "some json"
-        integration: "twitch-chat"
-      })
-      {
-        title
-        message
-        type
-        iconUrl
-        callToAction
-        link
-        timestamp
+  mutation CreateNotification($notification: NotificationInput) {
+    createNotification(notification: $notification) {
+      title
+      message
+      type
+      iconUrl
+      callToAction
+      link
+      timestamp
+      _id
+      data
+      source
+      user {
+        name
         _id
-        data
-        source
-        user {
-          name
-          _id
-        }
       }
+    }
   }
   `
 
-  const response = await request(process.env.INGEST_PATH, query)
-  console.log(`response: ${JSON.stringify(response)}`)
+  const variables = { 
+    notification: {
+      title: context['display-name'],
+      type: "Info",
+      data: JSON.stringify(context),
+      integration: "twitch-chat",
+      message
+    }
+  }
+
+  try {
+    await request(process.env.INGEST_PATH, query, variables)
+    console.log(`success sending twitch chat message to ingest server!`)
+  }
+  catch (error) {
+    console.log(`error sending notification from ${target} to the ingest server: ${error.stack}`)
+  }
 }
 
 // Called every time the bot connects to Twitch chat
