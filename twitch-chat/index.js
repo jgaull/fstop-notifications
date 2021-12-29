5291c72a8ecb59314cb106f0da904ad76d515a63
Jonathan Gaull <jonbeesh@gmail.com>
2021-12-29 13:02:56 -0800

    Updated to twitch-chat integration to new API structure
    Also changed all the spacing which masked every change I made...

--- twitch-chat/index.js
+++ twitch-chat/index.js
@@ -6,116 +6,105 @@ let activeIntegrations
 
 async function loadIntegrations() {
 
-  const query = gql`
-    query Integrations($filter: FilterFindManyIntegrationInput) {
-      integrations(filter: $filter) {
-        user
-        integrationSettings
-        _id
-      }
-    }
-  `
+	const query = gql`
+		query Integrations($filter: FilterFindManyIntegrationInput) {
+			integrations(filter: $filter) {
+				user {
+					_id
+				}
+				integrationSettings
+				_id
+			}
+		}
+	`
 
-  const variables = {
-    filter: {
-      type: "twitch-chat"
-    }
-  }
+	const variables = {
+		filter: {
+			type: "twitch_chat"
+		}
+	}
 
-  const response = await request(process.env.INGEST_PATH, query, variables)
-  activeIntegrations = response.integrations.map(integration => {
-    return {
-      _id: integration._id,
-      user: integration.user,
-      integrationSettings: JSON.parse(integration.integrationSettings)
-    }
-  })
+	const response = await request(process.env.INGEST_PATH, query, variables)
+	activeIntegrations = response.integrations.map(integration => {
+		return {
+			_id: integration._id,
+			user: integration.user,
+			integrationSettings: JSON.parse(integration.integrationSettings)
+		}
+	})
 
-  const channelsToMonitor = activeIntegrations
-  .map(integration => 
-    integration.integrationSettings.channelName)
-  .filter((channelName, index, channels) =>  
-    channels.indexOf(channelName) == index)
+	const channelsToMonitor = activeIntegrations
+	.map(integration => 
+		integration.integrationSettings.channelName)
+	.filter((channelName, index, channels) =>
+		channels.indexOf(channelName) == index)
 
-  const opts = {
-    channels: channelsToMonitor
-  }
-  
-  // Create a client with our options
-  console.log(`connecting to twitch channels: [${opts.channels}]`)
-  const client = new tmi.client(opts)
+	const opts = {
+		channels: channelsToMonitor
+	}
+	
+	// Create a client with our options
+	console.log(`connecting to twitch channels: [${opts.channels}]`)
+	const client = new tmi.client(opts)
 
-  // Register our event handlers (defined below)
-  client.on('message', onMessageHandler)
-  client.on('connected', onConnectedHandler)
+	// Register our event handlers (defined below)
+	client.on('message', onMessageHandler)
+	client.on('connected', onConnectedHandler)
 
-  // Connect to Twitch:
-  client.connect()
+	// Connect to Twitch:
+	client.connect()
 }
 
 // Called every time the bot connects to Twitch chat
 function onConnectedHandler (addr, port) {
-  console.log(`* Connected to ${addr}:${port}`)
+	console.log(`* Connected to ${addr}:${port}`)
 }
 
 // Called every time a message comes in
 async function onMessageHandler (target, context, message, self) {
-  /*
-  console.log('new chat message:')
-  console.log(`   target: ${JSON.stringify(target)}`)
-  console.log(`   context: ${JSON.stringify(context)}`)
-  console.log(`   msg: ${JSON.stringify(message)}`)
-  console.log(`   self: ${JSON.stringify(self)}`)
-  */
+	/*
+	console.log('new chat message:')
+	console.log(` target: ${JSON.stringify(target)}`)
+	console.log(` context: ${JSON.stringify(context)}`)
+	console.log(` msg: ${JSON.stringify(message)}`)
+	console.log(` self: ${JSON.stringify(self)}`)
+	*/
 
-  const query = gql`
-  mutation CreateNotification($record: CreateOneNotificationInput!) {
-    createNotification(record: $record) {
-      record {
-        title
-        message
-        type
-        iconUrl
-        callToAction
-        link
-        timestamp
-        data
-        source
-        integration
-        user
-        _id
-      }
-    }
-  }
-  `
+	const query = gql`
+	mutation CreateNotification($record: CreateOneNotificationInput!) {
+		createNotification(record: $record) {
+			recordId
+		}
+	}
+	`
 
-  const affectedIntegrations = activeIntegrations.filter(integration => {
-    return integration.integrationSettings.channelName.toLowerCase() == target.substring(1)
-  })
+	const affectedIntegrations = activeIntegrations.filter(integration => {
+		return integration.integrationSettings.channelName.toLowerCase() == target.substring(1)
+	})
 
-  const requests = affectedIntegrations.map(integration => {
+	const requests = affectedIntegrations.map(integration => {
 
-    const variables = {
-      record: {
-        title: context['display-name'],
-        type: "Info",
-        data: JSON.stringify(context),
-        integration: integration._id,
-        user: integration.user,
-        message
-      }
-    }
-    
-    return request(process.env.INGEST_PATH, query, variables)
-  })
+		const variables = {
+			record: {
+				title: context['display-name'],
+				type: "Info",
+				data: JSON.stringify(context),
+				integration: integration._id,
+				user: integration.user._id,
+				message
+			}
+		}
+		
+		return request(process.env.INGEST_PATH, query, variables)
+	})
 
-  try {
-    await Promise.all(requests)
-    console.log(`success sending twitch chat message to ingest server!`)
-  }
-  catch (error) {
-    console.log(`error sending notification from ${target} to the ingest server: ${error.stack}`)
-  }
+	try {
+		await Promise.all(requests)
+		console.log(`success sending twitch chat message to ingest server!`)
+	}
+	catch (error) {
+		console.log(`error sending notification from ${target} to the ingest server: ${error.stack}`)
+	}
 }
 
 loadIntegrations()
