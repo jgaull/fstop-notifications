035dde216f8aa5b45a756ed2293c1196a988a538
Jonathan Gaull <jonbeesh@gmail.com>
2022-01-03 19:45:48 -0800

    Properly merged index.js

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ const { Mongo } = require('@accounts/mongo')
 const { AccountsServer } = require('@accounts/server')
 const { AccountsPassword } = require('@accounts/password')
 const { AccountsModule } = require('@accounts/graphql-api')
-
-const { ApolloServer } = require('apollo-server')
-
-const buildSchema = require('./schema')
+const { ApolloServer } = require('apollo-server-express')
+const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core')
+const express = require('express')
+const http = require('http')
+const { execute, subscribe } = require('graphql')
+const { SubscriptionServer } = require('subscriptions-transport-ws')
 
 // making this a dictionary because I feel like that will be useful later on.
 const modelRegistry = {
@@ -19,9 +21,15 @@ const modelRegistry = {
     Integration: require('./models/integration')
 }
 
+const buildSchema = require('./schema')
+
+// Required logic for integrating with Express
+const app = express()
+const httpServer = http.createServer(app)
+
 async function main() {
 
-    await mongoose.connect('mongodb://localhost:27017/fstop-notifications')
+    await mongoose.connect(process.env.DATABASE_URL)
 
     const accountsServer = new AccountsServer({
         // We link the mongo adapter we created in the previous step to the server
@@ -38,52 +46,52 @@ async function main() {
     const accountsGraphQL = AccountsModule.forRoot({ accountsServer });
 
     // When we instantiate our Apollo server we use the schema and context properties
-    const server = new ApolloServer({
-        schema: buildSchema(modelRegistry, accountsGraphQL.schema),
-        context: accountsGraphQL.context,
+
+    const schema = buildSchema(modelRegistry, accountsGraphQL.schema)
+
+    const subscriptionServer = SubscriptionServer.create({
+        // This is the `schema` we just created.
+        schema,
+        // These are imported from `graphql`.
+        execute,
+        subscribe,
+        }, {
+        // This is the `httpServer` we created in a previous step.
+        server: httpServer,
+        // Pass a different path here if your ApolloServer serves at
+        // a different path.
+        path: '/'
     })
-const { ApolloServer } = require('apollo-server-express')
-const { ApolloServerPluginDrainHttpServer } = require('apollo-server-core')
-const express = require('express')
-const http = require('http')
-const { execute, subscribe } = require('graphql')
-const { SubscriptionServer } = require('subscriptions-transport-ws')
 
-// Required logic for integrating with Express
-const app = express()
-const httpServer = http.createServer(app)
-
-const subscriptionServer = SubscriptionServer.create({
-    // This is the `schema` we just created.
-    schema,
-    // These are imported from `graphql`.
-    execute,
-    subscribe,
- }, {
-    // This is the `httpServer` we created in a previous step.
-    server: httpServer,
-    // Pass a different path here if your ApolloServer serves at
-    // a different path.
-    path: '/'
- })
-
- const server = new ApolloServer({
-    plugins: [
-        require('./apollo-authentication-plugin'),
-        ApolloServerPluginDrainHttpServer({ httpServer }),
-        {
-            async serverWillStart() {
-              return {
-                async drainServer() {
-                  subscriptionServer.close()
+    const server = new ApolloServer({
+        plugins: [
+            ApolloServerPluginDrainHttpServer({ httpServer }),
+            {
+                async serverWillStart() {
+                    return {
+                    async drainServer() {
+                        subscriptionServer.close()
+                    }
+                    }
                 }
-              }
             }
-        }
-    ],
-    schema
-})
+        ],
+        schema,
+        context: accountsGraphQL.context
+    })
 
+    await server.start();
+    server.applyMiddleware({
+        app,
+        path: '/'
+    });
+
+    // The `listen` method launches a web server.
+    await new Promise(resolve => httpServer.listen({ port: process.env.PORT }, resolve))
+    console.log(`🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`)
+}
+
+/*
 async function main() {
 
     await mongoose.connect(process.env.DATABASE_URL)
@@ -98,5 +106,6 @@ async function main() {
     await new Promise(resolve => httpServer.listen({ port: process.env.PORT }, resolve))
     console.log(`🚀 Server ready at http://localhost:${process.env.PORT}${server.graphqlPath}`)
 }
+*/
 
 main().catch(err => console.log(`error initializing the server: ${err.stack}`))
